228ece31c7d3b7e9cc81c06fca044445dcdc28a8
Mukesh <mukesh.sumit20@gmail.com>
2011-04-20 19:42:32 +0530

    Generate Product Model and update Layouts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myshopping::Application.config.secret_token = '5aba52bee3a09fa58dd58c2bf37fecec48fc5dbd348661fd7baabf4aa7d24fde08ee230cb43874f6cecbced36471fe39a38f5c72c484aa05a12969894f400a62'
